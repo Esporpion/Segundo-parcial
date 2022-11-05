76473cc868a780108e7468a43add156295d9b570
Esporpion <116697818+Esporpion@users.noreply.github.com>
2022-11-04 23:52:45 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//El scritp nos permite hacer la animacion de mover el bloque que cubre las 2 opciones de iniciar sesion y registrarse
 $("#signup").click(function() {
   $(".message").css("transform", "translateX(100%)");
   if ($(".message").hasClass("login")) {
